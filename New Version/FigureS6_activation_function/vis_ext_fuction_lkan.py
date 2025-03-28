e08d7b3636887fe9c27eefa627769b39a2a05403
WangJiawenChristy <wjw1116@connect.hku.hk>
2025-03-28 18:35:03 +0800

    Update vis_ext_fuction_lkan.py

--- New Version/FigureS6_activation_function/vis_ext_fuction_lkan.py
+++ New Version/FigureS6_activation_function/vis_ext_fuction_lkan.py
@@ -14,15 +14,15 @@ model.load_state_dict(torch.load('New Version/model/lorentkan5_kf1_noise0_r4_1_5
 model.to(device)
 model.eval()
 
-data = pd.read_csv(r'D:\projects\CEST-KAN0725\cestkanpc10\zspec_test_2subjects.csv', header=None)
-targets = pd.read_csv(r'D:\projects\CEST-KAN0725\cestkanpc10\param_test_2subjects.csv', header=None)
+data = pd.read_csv(r'zspec_test_2subjects.csv', header=None)
+targets = pd.read_csv(r'param_test_2subjects.csv', header=None)
 data_arr = np.array(data).astype(np.float32)
 targets_arr = np.array(targets).astype(np.float32)
   
 # 创建张量并移动到GPU
 test_data_tensor = torch.tensor(data_arr).to(device)
 test_targets_tensor = torch.tensor(targets_arr).to(device)
-test_data_tensor_mean = torch.mean(test_data_tensor, dim=0).view(1, -1).to(device)  # 移动到GPU
+test_data_tensor_mean = torch.mean(test_data_tensor, dim=0).view(1, -1).to(device)  
 
 # 调用plot_spline
 model.plot_loren(test_data_tensor_mean)

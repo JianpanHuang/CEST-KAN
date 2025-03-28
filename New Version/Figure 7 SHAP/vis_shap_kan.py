997bcd8ff878e250515de257140fd6f4aa7c8d10
WangJiawenChristy <wjw1116@connect.hku.hk>
2025-03-28 18:17:40 +0800

    Update and rename vis_shap_kan_manu.py to vis_shap_kan.py

--- New Version/Figure 7 SHAP/vis_shap_kan.py
+++ New Version/Figure 7 SHAP/vis_shap_kan.py
@@ -12,7 +12,7 @@ num_grids = 9
 spline_order = 3
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  # 使用 GPU，如果可用
 model = KAN([43, 100, 9], num_grids=num_grids, spline_order=spline_order)
-model_path = "D:\projects\CEST-KAN0725\cestkanpc10\code\kan_v3_kan_kf_gs9_model\kan5_kf1_noise0_r1kf1_100_1_52_2484.pth"
+model_path = "New Version/model/kan5_kf1_noise0_r3kf1_100_1_48_2409.pth"
 model.load_state_dict(torch.load(model_path, map_location=device))
 model.to(device)
 model.eval()
